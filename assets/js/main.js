fa4a16084acc5287fc00df6f85b0620485b9c4be
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2021-08-26 21:41:57 +0200

    add JS and menu logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,32 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.querySelector('.nav-menu');
+const navToggle = document.querySelector('.nav-toggle');
+const navClose = document.querySelector('.nav-close');
 
+//menu show
+if(navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+//menu hidden
+if(navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+//after click on list element our menu hide
 
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+  const navMenu = document.querySelector('.nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(el => el.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
