a694e8ce725040fbadc67c03933d9b3fd1555424
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2021-09-06 07:11:44 +0200

    Add active-link JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,7 +64,25 @@ function scrollUp() {
 
 window.addEventListener('scroll', scrollUp)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const section = document.querySelectorAll('section[id]')
 
+function scrollActiv() {
+  const scrollY = window.pageYOffset
+
+  section.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActiv)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
